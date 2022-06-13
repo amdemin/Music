fcbcebac33c90b8239e9c51040cd382ba5b9c4a6
artem404 <divine_noodle@mail.ru>
2022-06-13 12:34:07 +0300

    Kamilla v1

--- main.py
+++ main.py
@@ -11,31 +11,7 @@ from PIL import Image
 from boto.s3.connection import S3Connection
 import os
 from telegram_bot_pagination import InlineKeyboardPaginator
-import urllib.request as urllib2
-import time
-import re
-import random as rd
-import vk_api
-from vk_api.audio import VkAudio
 import traceback
-
-def convert_photos(message):
-    try:
-        file_info = bot.get_file(message.photo[-1].file_id)
-        downloaded_file = bot.download_file(file_info.file_path)
-        return downloaded_file
-    except Exception:
-        pass
-    return None
-
-def convert_videos(message):
-    try:
-        file_info = bot.get_file(message.video.file_id)
-        downloaded_file = bot.download_file(file_info.file_path)
-        return downloaded_file
-    except Exception:
-        pass
-    return None
 
 
 # random name for mediafile to make it uniquely identified
@@ -58,21 +34,12 @@ def random_name():
 # retrieve values from Config Vars in Heroku
 tg_token = os.getenv('tg_token')
 dbx_token = os.getenv('dbx_token')
-vk_login = os.getenv('vk_login')
-vk_password = os.getenv('vk_password')
-
-
-# vk_session = vk_api.VkApi(vk_login, vk_password)
-# vk_session.auth(token_only=True)
-# vk = vk_session.get_api()
-# vk_audio = VkAudio(vk_session)
 
 # initiate telegram api
 bot = telebot.TeleBot(tg_token)
 # initiate dropbox api
 dbx = dropbox.Dropbox(dbx_token)
-#music_dict = {'#1' : 'cream soda', '#2' : 'lxst cxntury', '#3': 'f3rctak', '#4': 'low pulse', '#5': 'vacuum',
-# '#6': 'don diablo', '#7': 'vvpskvd', '#8': 'phonk', '#9': 'apple music', '#10': 'monolithic'}
+
 music_dict = {}
 folders = dbx.files_list_folder("/Music").entries
 # define tags for every folder of the music cloud
@@ -89,250 +56,28 @@ def start_command(message):
 def help_command(message):
     bot.send_message(message.from_user.id, "Enter the name of artist or song correctly, e.g: Сream Soda")
 
-@bot.message_handler(commands=['about'])
-def about_command(message):
-    try:
-        about_keyboard = types.InlineKeyboardMarkup()
-        button = types.InlineKeyboardButton(text="Github", url="https://github.com/artem404/Music")
-        about_keyboard.add(button)
-        bot.send_message(message.chat.id, "The bot was made by Artem @artem_tg \nThis project can be found on: ", reply_markup=about_keyboard)
-    except Exception as e:
-        print(e)
-        pass
-
-    
-@bot.message_handler(commands=['shot'])
-def ask_word(message):
-    try:
-        bot.send_message(message.chat.id, "Enter a password for your files")
-        bot.register_next_step_handler(message, shot_command)
-    except Exception as e:
-        print(e)
-        pass
-        
-def shot_command(message):
-    try:
-        word = message.text
-        input = message.content_type
-        if input == 'text':
-            if word == "/screen":
-                photo_command(message)
-            elif word == "/shot":
-                ask_word(message)
-            elif word == "/start":
-                start_command(message)
-            elif word == "/help":
-                help_command(message)
-            elif word == "/about":
-                about_command(message)
-            elif word == "/delete":
-                delete_command(message)
-            else:
-                flag = False
-                keyboard = types.InlineKeyboardMarkup()
-                photos = []
-
-    #         entries = Path('Photos/')
-    #         for entry in entries.iterdir():
-
-                for entry in dbx.files_list_folder('/Host').entries:
-                    photos.append(entry.name)
-                num = 0
-                print(photos)
-                for name_photo in range(0, len(photos)):
-                    separate = photos[name_photo].split(".")  # to extract the password and format
-                    format = separate[len(separate) - 1]
-                    if format.lower() == "mp4":
-                        if word.lower() == separate[0].lower():
-                            button = types.InlineKeyboardButton(text=separate[1], callback_data="video" + str(num))
-                            keyboard.add(button)
-                            flag = True
-                    elif format.lower() == "jpg":
-                        if word.lower() == separate[0].lower():
-                            button = types.InlineKeyboardButton(text=separate[1], callback_data="photo" + str(num))
-                            keyboard.add(button)
-                            flag = True
-                    num += 1
-                if flag:
-                    bot.send_message(message.chat.id, "Found: ", reply_markup=keyboard)
-                else:
-                    bot.send_message(message.chat.id, "No results with this password, you may try again /shot")
-        elif input == 'photo':
-            bot.send_message(message.chat.id, "You sent a photo, not a password, you may try again /shot")
-        elif input == 'video':
-            bot.send_message(message.chat.id, "You sent a video, not a password, you may try again /shot")
-        else:
-            bot.send_message(message.chat.id, "You should enter a word, you may try again /shot")
-    except Exception as e:
-        print(e)
-        pass
-
-    
-@bot.message_handler(commands=['screen'])
-def photo_command(message):
-    try:
-        bot.send_message(message.from_user.id, "Create a password for your files")
-        bot.register_next_step_handler(message, reply_command)
-    except Exception as e:
-        print(e)
-        pass
-
-def reply_command(message):
-    try:
-        word = message.text
-        input = message.content_type
-        if input == "text":
-            if word == "/screen":
-                photo_command(message)
-            elif word == "/shot":
-                ask_word(message)
-            elif word == "/start":
-                start_command(message)
-            elif word == "/help":
-                help_command(message)
-            elif word == "/about":
-                about_command(message)
-            elif word == "/delete":
-                delete_command(message)
-            else:
-                bot.send_message(message.from_user.id, "Sent as media, strictly one by one, otherwise will not work")
-                bot.register_next_step_handler(message, process_photo, word)
-        else:
-            bot.send_message(message.from_user.id, "Firstly, create the password")
-            bot.register_next_step_handler(message, reply_command)
-    except Exception as e:
-        print(e)
-        pass
-
-def process_photo(message, word):
-    try:
-        input = message.content_type
-        if input == 'text':
-            if message.text == "/screen":
-                photo_command(message)
-            elif message.text == "/shot":
-                ask_word(message)
-            elif message.text == "/start":
-                start_command(message)
-            elif message.text == "/help":
-                help_command(message)
-            elif message.text == "/about":
-                about_command(message)
-            elif message.text == "/delete":
-                delete_command(message)
-            else:
-                bot.send_message(message.from_user.id, "You sent words, you may try again /screen")
-        elif input == 'document':
-            bot.send_message(message.from_user.id, "You send photo as file")
-            bot.send_message(message.from_user.id, "Please, send as photo")
-            bot.register_next_step_handler(message, process_photo, word)
-        elif input == 'photo':
-            bot.send_message(message.from_user.id, "Please, wait ...")
-            content = convert_photos(message)
-            rand_name = random_name()
-            file_name = word + "." + rand_name
-            dbx.files_upload(content, "/Host/" + file_name + ".jpg")
-            bot.send_message(message.from_user.id, "Photo has been uploaded")
-            bot.register_next_step_handler(message, process_photo, word)
-        elif input == 'video':
-            bot.send_message(message.from_user.id, "Please, wait ...")
-            content = convert_videos(message)
-            rand_name = random_name()
-            file_name = word + "." + rand_name
-            dbx.files_upload(content, "/Host/" + file_name + ".mp4")
-            bot.send_message(message.from_user.id, "Video has been uploaded")
-            bot.register_next_step_handler(message, process_photo, word)
-        else:
-            bot.send_message(message.from_user.id, "Please, enter the command again to upload files /screen")
-    except Exception as e:
-        print(e)
-
-        
-@bot.message_handler(commands=['delete'])
-def delete_command(message):
-    try:
-        bot.send_message(message.from_user.id, "Enter a password for your files, they will be deleted from a database")
-        bot.register_next_step_handler(message, delete_process)
-    except Exception as e:
-        print(e) 
-
-def delete_process(message):
-    try:
-        word = message.text
-        if word == "/screen":
-                photo_command(message)
-        elif word == "/shot":
-            ask_word(message)
-        elif word == "/start":
-            start_command(message)
-        elif word == "/help":
-            help_command(message)
-        elif word == "/about":
-            about_command(message)
-        elif word == "/delete":
-            delete_command(message)
-        else:
-            num = 0
-            for entry in dbx.files_list_folder('/Host').entries:  
-                separate = entry.name.split('.')
-                if word == separate[0]:
-                    dbx.files_delete("/Host/" + entry.name)
-                    num += 1
-            if 0 < num < 2:
-                bot.send_message(message.from_user.id, str(num) + " file '" + word + "' was successfully deleted")
-            elif num > 1:
-                bot.send_message(message.from_user.id, str(num) + " files '" + word + "' were successfully deleted")
-            else:
-                bot.send_message(message.from_user.id, "No results with this password, you may try again /delete")
-    except Exception as e:
-        print(e)
 
 @bot.message_handler(content_types=['text', 'audio'])
 def process_input(message, page=1, text=""):
     text = message.text
     try:
 
-        # this code is used to identify repeated requests from one input
-        text = text.split("• ")
+        text = text.split("^ ")
         if len(text) > 1:
             text = text[1]
         else:
             text = text[0]
 
+        
         keyboard = types.InlineKeyboardMarkup()
 
-        # retrieve songs from the private dropbox cloud
-        if 'dropbox' in text:
-            cut_text = text.strip('dropbox')
-            result = extract_music_files(cut_text, music_dict)
-            # split list into small sublists limited by 10
-            result_split_page = [result[x:x+10] for x in range(0, len(result), 10)]
-            # add button to telegram according to page numeration
-            page_file = page-1
-        # retrieve songs from public vk
-        else:
-
-            # autorisation process
-            vk_session = vk_api.VkApi(vk_login, vk_password)
-            vk_session.auth(token_only=True)
-            vk = vk_session.get_api()
-            vk_audio = VkAudio(vk_session)
-            ###
-
-            result = []
-            page_file = 0
-            number_of_songs = len(list(vk_audio.search(text, count=15, offset=0)))
-            time.sleep(rd.uniform(0.5,1.1))
-            tracks = list(vk_audio.search(text, count=5, offset=5*(page-1)))
-            for song in range(0, len(tracks)):
-                row = []
-                row.append(tracks[song]['artist'] + ' ' + tracks[song]['title'])
-                row.append(tracks[song]['owner_id'])
-                row.append(tracks[song]['id'])
-                result.append(row)
-            result_split_page = [result[x:x+5] for x in range(0, number_of_songs, 5)]
-
-        print(result)
+        # cut_text = text.strip('dropbox')
+        cut_text = text
+        result = extract_music_files(cut_text, music_dict)
+        # split list into small sublists limited by 10
+        result_split_page = [result[x:x+10] for x in range(0, len(result), 10)]
+        # add button to telegram according to page numeration
+        page_file = page-1
 
         audio_format = ".mp3"
         if len(result_split_page) != 0:  
@@ -354,11 +99,12 @@ def process_input(message, page=1, text=""):
                 bot.send_message(message.chat.id, "Page: " + str(page) + " • " + str(text), reply_markup=paginator.markup, parse_mode='Markdown')
         else:
             bot.send_message(message.chat.id, "No results")
+
     except Exception as e:
-        print('Error in process_input:', e)
-        print(traceback.format_exc())
+        print(e)
         # bot.send_message(message.from_user.id, e)
         bot.send_message(message.chat.id, "Error occured, no results")
+
 
 # this callback handler is only for paginator requests
 @bot.callback_query_handler(func=lambda call: call.data.split(';')[0]=='page')
@@ -438,5 +184,5 @@ def callback_inline(call):
             bot.send_photo(call.from_user.id, file_id)
         else:
             pass
-
+        
 bot.polling(none_stop=True, interval=1)
--- music_process.py
+++ music_process.py
@@ -21,14 +21,7 @@ def check_text(text):
 
 def extract_music_files(text, music_dict):
     try:
-        # folders = dbx.files_list_folder("/Music").entries
-        # folder_names= []
-        
-        # to retrieve folder names from folder metadata
-        #for i in range(0, len(folders)):
-        #    folder_names.append(folders[i].name)
 
-        # check f3rctak unique name
         text = check_text(text)
 
         data = []
@@ -54,11 +47,41 @@ def extract_music_files(text, music_dict):
         data.sort()
         return data
 
+        # # folders = dbx.files_list_folder("/Music").entries
+        # # folder_names= []
+        
+        # # # to retrieve folder names from folder metadata
+        # # for i in range(0, len(folders)):
+        # #     folder_names.append(folders[i].name)
+
+        # # text = check_text(text)
+
+        # data = []
+        # # to search songs by input
+        # for i in range(0, len(folder_names)):
+        #     # dropbox search function (path, request)
+        #     result = dbx.files_search("/Music/" + folder_names[i], text)
+        #     # song metadata
+        #     result = result.matches
+        #     if result != []:
+        #         for k in range(0, len(result)):
+        #             row = []
+        #             # retrieve song name from metadata
+        #             row.append(result[k].metadata.name)
+
+        #             # to get the right tag whatever the order is
+        #             for tag, value in music_dict.items():
+        #                 if value == folder_names[i]:
+        #                     row.append(tag)
+        #             data.append(row)
+        # data.sort()
+        # return data
+
     except Exception as e:
         print(e)
         pass
 
-# retrieve value from config vars in Heroku
+
 dbx_token = os.getenv('dbx_token')
 # initiate dropbox api
 dbx = dropbox.Dropbox(dbx_token)
